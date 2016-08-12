3af1d978ca86c965f60c501ab173bc27408092b6
JARVIS <1151770629@qq.com>
2016-08-12 09:25:51 +0800

    初始化方块 绝对定位

--- test/js/platform.js
+++ test/js/platform.js
@@ -17,31 +17,12 @@ $(document).ready(function () {
     });
 });
 window.onload = main;
-var brick_width = 50;
-var brick_height = 20;
 var move_speed = 10;
 var min_btn_position = 0;
 var max_btn_position = getWidth();
 function main() {
-    //获得页面的宽度和高度，计算出方块的数量
-    var brick_row_num = parseInt(getWidth() / brick_width);
-    //初始化页面 生成方块
-//            var brick_line_num = parseInt(50);
-    var board = $(".board");
-    var ii = 0;
-    var jj = 0;
-    for (; ii < 10; ii++) {
-        var row_brick = $(document.createElement("div"));
-        row_brick.addClass("rowbrick");
-        for (jj = 0; jj < 10; jj++) {
-            var j_brick = $(document.createElement("span"));
-            j_brick.addClass("red block float_left");
-            row_brick.append(j_brick);
-        }
-        board.append(row_brick);
-    }
-    board.addClass("black");
-    show_info(getWidth(), getHeight(), brick_row_num);
+    creat_brick();//生成方块
+    show_info(getWidth(), getHeight());
 }
 function getWidth() {
     return document.body.clientWidth;
@@ -66,4 +47,23 @@ function btn_move_right() {
     var btn_x = button.offsetLeft + move_speed;
     if (btn_x > 1699)btn_x = max_btn_position - 200;
     button.style.left = btn_x + "px";
+}
+//初始化方块
+function creat_brick(){
+    var row_num=10;//一行的方块的个数
+    var line_num=5;//一列的方块的个数
+    var brick_id=0;//砖块的id
+    var brick_left=300;//砖块离左侧边距的位置
+    var brick_top=20;//砖块离上边距的位置
+    var i=0;
+    var j=0;
+    for(;i<line_num;i++){
+        for(j=0;j<row_num;j++){
+            //求id
+            brick_id=i*10+j+1;
+            brick_left=300+70*j;
+            brick_top=20+40*i;
+            document.write("<div class='brick red' id='"+brick_id+"' style='position: absolute;left: "+brick_left+"px;top: "+brick_top+"px;'></div>");
+        }
+    }
 }
